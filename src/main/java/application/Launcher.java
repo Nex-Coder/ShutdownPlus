9f1e5f7ed14b594d917b32d975add230e4ae7bc3
Nex-Coder <callum.cunha@gmail.com>
2022-01-25 23:41:23 +0000

    First Template

---
+++ src/main/java/application/Launcher.java
@@ -0,0 +1,19 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+class Launcher {
+    public static void main(String[] args) {
+        Application.launch(MainStart.class);
+    }
+
+    public static class MainStart extends Application {
+        @Override
+        public void start(Stage stg) {
+
+        }
+    }
+}
+
+
